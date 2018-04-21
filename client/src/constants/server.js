0783123272ebc3def258af9d30425691a0314254
sandu@ucdavis.edu <sandu@ucdavis.edu>
2018-04-21 10:15:25 -0700

    trying to fix stripe

--- client/src/constants/server.js
+++ client/src/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-    ? 'http://myapidomain.com'
+    ? 'https://morning-falls-62928.herokuapp.com/'
     : 'http://localhost:8080';
 
 export default PAYMENT_SERVER_URL;
